c0e44b04310b44614095c576fb96595ad46ad0b4
Jasmon Goeckemeyer <goeckemeyerjasmon@gmail.com>
2023-05-07 14:59:29 +0200

    first interation of the script

---
+++ gpio_control.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+# die GPIOS koennen über die PINNummer oder die GPIONummer
+# angesprochen werden
+GPIO.setmode(GPIO.BCM) # hier GPIO-Nummer
+GPIO.setup(18, GPIO.OUT)
+ 
+interval_in_s = 5.0
+while(True):
+	print("off")
+	GPIO.output(18, GPIO.HIGH)
+	time.sleep(interval_in_s) # sleep interval in Sekunden
+	print("on")
+	GPIO.output(18, GPIO.LOW)
+	time.sleep(interval_in_s)
